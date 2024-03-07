e3938c35e9f00e5e49f66982c102fc582ffccd54
Yuta Matsuo <y.matsuo.dev@gmail.com>
2024-03-07 15:36:26 +0900

    fix: use ComponentProps on Button

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -5,16 +5,14 @@ import styles from '@/components/ui/Button/index.module.css';
 import { Icon } from '@/components/ui/Icon';
 
 import type { IconName } from '@/components/ui/Icon/type';
+import type { ComponentProps } from 'react';
 
 type Props = {
   theme: 'primary' | 'primaryOutlined' | 'secondary' | 'secondaryOutlined' | 'paid' | 'danger' | 'dangerOutline';
   label: string;
-  type: 'button' | 'submit';
-  onClick: () => void;
-  disabled?: boolean;
   iconName?: IconName;
   fullWidth?: boolean;
-};
+} & ComponentProps<'button'>;
 
 const THEME_CLASSES: { [key in Props['theme']]: string } = {
   primary: styles.primary,
@@ -27,14 +25,12 @@ const THEME_CLASSES: { [key in Props['theme']]: string } = {
 } as const;
 
 export const Button = forwardRef<HTMLButtonElement, Props>(
-  ({ theme = 'primary', label, type, onClick, disabled, iconName, fullWidth }, ref) => {
+  ({ theme = 'primary', label, iconName, fullWidth, ...buttonProps }, ref) => {
     return (
       <button
+        {...buttonProps}
         className={clsx(styles.buttonContainer, THEME_CLASSES[theme], fullWidth ? styles.fullWidth : '')}
         ref={ref}
-        type={type}
-        disabled={disabled}
-        onClick={onClick}
       >
         {/* TODO: sizeは文字サイズに合わせて要調整 */}
         {iconName !== undefined && <Icon name={iconName} size={16} />}
